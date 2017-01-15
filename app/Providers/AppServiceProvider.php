11a2adc8859ae488d99fb64e33c0701bfe270f2d
Katy Ereira <katye@deeson.co.uk>
2017-01-15 15:41:35 +0000

    Register `TwitterStream` with `AppServiceProvider` using environment variables.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\ActivityStreams\TwitterStream;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind('App\ActivityStreams\TwitterStream', function ($app) {
+            return new TwitterStream(
+              env('TWITTER_ACCESS_TOKEN', null),
+              env('TWITTER_ACCESS_TOKEN_SECRET', null),
+              \Phirehose::METHOD_FILTER
+            );
+        });
     }
 }
