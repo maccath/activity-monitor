9ebb1cf5cb56f7667ade68d793a070f541c2917d
Katy Ereira <katye@deeson.co.uk>
2017-01-17 15:30:21 +0000

    Travis CI -> DO automatic deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,6 @@
 set -x
 #if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
-    cd _site
-    git init
-
     git remote add deploy "deploy@138.68.172.224:/var/www/Sites/activity-monitor"
     git config user.name "Travis CI"
     git config user.email "katy.ereira+travisCI@gmail.com"
