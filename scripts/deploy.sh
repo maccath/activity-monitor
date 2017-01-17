625cdf8b722e1522eb2639a4c2d71069beda49e7
Katy Ereira <katye@deeson.co.uk>
2017-01-17 15:33:23 +0000

    Travis CI -> DO automatic deploy.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,7 @@ set -x
 #if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
     git remote add deploy "deploy@138.68.172.224:/var/www/Sites/activity-monitor"
-    git config user.name "Travis CI"
-    git config user.email "katy.ereira+travisCI@gmail.com"
-
-    git add .
-    git commit -m "Deploy"
-    git push --force deploy master
+    git push --force deploy $TRAVIS_BRANCH
 #else
 #    echo "Not deploying, since this branch isn't master."
 #fi
