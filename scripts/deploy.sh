59e68a12f99c41af6e064cfa19931ead4365c35c
Katy Ereira <katye@deeson.co.uk>
2017-01-18 18:27:08 +0000

    Update hostname.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 set -x
 #if [ $TRAVIS_BRANCH == 'master' ] ; then
     # Initialize a new git repo in _site, and push it to our server.
-    git remote add deploy "deploy@138.68.172.224:/var/www/Sites/activity-monitor"
+    git remote add deploy "deploy@katy-ereira.co.uk:/var/repo/activity-monitor.git"
     git push --force deploy $TRAVIS_BRANCH
 #else
 #    echo "Not deploying, since this branch isn't master."
