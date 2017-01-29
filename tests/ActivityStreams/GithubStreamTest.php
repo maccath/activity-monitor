d255c1fb7dd90457db80a954b9d4277a075784ca
Katy Ereira <katye@deeson.co.uk>
2017-01-29 15:15:41 +0000

    Fix GithubStream tests by using mocked API calls.

--- tests/ActivityStreams/GithubStreamTest.php
+++ tests/ActivityStreams/GithubStreamTest.php
@@ -7,11 +7,32 @@ class GithubStreamTest extends TestCase
     /** @var \App\ActivityStreams\GithubStream */
     private $githubStream;
 
+    /** @var string fake GitHub API response body */
+    private $fakeResponseBody;
+
     public function setUp()
     {
         parent::setUp();
 
-        $this->githubStream = resolve(\App\ActivityStreams\GithubStream::class);
+        $client = $this->getMockBuilder(\GuzzleHttp\Client::class)
+          ->disableOriginalConstructor()
+          ->setMethods(['get'])
+          ->getMock();
+
+        $response = $this->getMockBuilder(\GuzzleHttp\Psr7\Response::class)
+          ->disableOriginalConstructor()
+          ->setMethods(['getBody'])
+          ->getMock();
+
+        $this->fakeResponseBody = $this->getMockBuilder(\GuzzleHttp\Psr7\Stream::class)
+          ->disableOriginalConstructor()
+          ->setMethods(['getContents'])
+          ->getMock();
+
+        $client->method('get')->willReturn($response);
+        $response->method('getBody')->willReturn($this->fakeResponseBody);
+
+        $this->githubStream = new \App\ActivityStreams\GithubStream($client);
     }
 
     public function testImplementsPeriodicStream()
@@ -21,6 +42,8 @@ class GithubStreamTest extends TestCase
 
     public function testFetchGetsLatestItems()
     {
+        $this->fakeResponseBody->method('getContents')->willReturn($this->getFakeGithubActivity());
+
         // First fetch, all items
         $this->githubStream->fetch();
         $this->assertNotEmpty($this->githubStream->getItems());
@@ -32,8 +55,43 @@ class GithubStreamTest extends TestCase
 
     public function testNewActivityDispatchesJob()
     {
+        $this->fakeResponseBody->method('getContents')->willReturn($this->getFakeGithubActivity());
+
         $this->expectsJobs([\App\Jobs\ProcessGithubActivity::class]);
 
         $this->githubStream->fetch();
+    }
+
+    public function getFakeGithubActivity()
+    {
+        return '[
+          {
+            "type": "Event",
+            "public": true,
+            "payload": {
+            },
+            "repo": {
+              "id": 3,
+              "name": "octocat/Hello-World",
+              "url": "https://api.github.com/repos/octocat/Hello-World"
+            },
+            "actor": {
+              "id": 1,
+              "login": "octocat",
+              "gravatar_id": "",
+              "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+              "url": "https://api.github.com/users/octocat"
+            },
+            "org": {
+              "id": 1,
+              "login": "github",
+              "gravatar_id": "",
+              "url": "https://api.github.com/orgs/github",
+              "avatar_url": "https://github.com/images/error/octocat_happy.gif"
+            },
+            "created_at": "2011-09-06T17:26:27Z",
+            "id": "12345"
+          }
+        ]';
     }
 }
