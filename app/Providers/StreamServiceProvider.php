56c53b90cfd74300322ac1f2d345ef7f0e17162e
Katy Ereira <katye@deeson.co.uk>
2017-01-29 18:42:24 +0000

    Move streams to separate `StreamServiceProvider`

---
+++ app/Providers/StreamServiceProvider.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Providers;
+
+use App\ActivityStreams\GithubStream;
+use App\ActivityStreams\TwitterStream;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use Illuminate\Support\ServiceProvider;
+
+class StreamServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $streamProviders = [
+            TwitterStream::class,
+            GithubStream::class,
+        ];
+
+        $this->app->tag($streamProviders, 'streams');
+
+        $this->app->bind(TwitterStream::class, function () {
+            return new TwitterStream(
+              env('TWITTER_ACCESS_TOKEN', null),
+              env('TWITTER_ACCESS_TOKEN_SECRET', null),
+              \Phirehose::METHOD_FILTER
+            );
+        });
+
+        $this->app->when(GithubStream::class)
+            ->needs(ClientInterface::class)
+            ->give(function () {
+                return new Client([
+                'base_uri' => 'https://api.github.com',
+                'timeout' => 2.0,
+                'headers' => [
+                  'Authorization' => 'token ' . env('GITHUB_ACCESS_TOKEN', null)
+                ]
+            ]);
+        });
+    }
+}
